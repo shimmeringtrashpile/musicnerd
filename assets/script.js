16bfb43a52110a4427e90d3b6ee7e3aa42e637e9
Rob Ray <rob.ray@jpl.nasa.gov>
2020-10-04 16:38:46 -0700

    Light up annotation when the player timeline hits that mark.

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,150 @@
-const mainTrack = document.getElementById('main-track');
-const timeStamps = document.getElementById('time-stamps');
-const play = document.querySelector('.play');
-const pause = document.querySelector('.pause');
-const trackDuration = document.querySelector('.duration');
+const Program = {
+    init(){
+        this.mainTrack = document.getElementById('main-track');
+        this.timeStamps = document.getElementById('time-stamps');
+        this.play = document.querySelector('.play');
+        this.pause = document.querySelector('.pause');
+        this.trackDuration = document.querySelector('.duration');
+        
+        this.annotations = [{
+                time: '00:05',
+                note: 'transition.',
+                link: 'http://www.google.com'
+            },    
+        {
+            time: '09:13',
+            note: 'It speeds up here unexpectedly.',
+            link: 'http://www.google.com'
+        },
+        {
+            time: '09:15',
+            note: 'It speeds up here too.',
+            link: 'http://www.google.com'
+        },
+        {
+            time: '14:23',
+            note: 'Listen for the change here. The strings fade out.',
+        }];
+        this.events();
+        this.renderAnnotations();
+        this.setCurrentAnnotationInterval();
+    },
+    
+    events(){
+        const click = 'click'
+        this.play.addEventListener(click, (event) => {
+            // press play and hide play and show pause
+            this.playTrack();
+        })
+        
+        this.pause.addEventListener(click, (event) => {
+            // press play and hide play and show pause
+            this.pauseTrack();
+        })
+        
+        this.timeStamps.addEventListener(click, (event) => {
+            console.log(event, event.target);
+            //event.target.dataset.time;
+            const dataTime = event.target.getAttribute('data-time');
+            this.mainTrack.currentTime = dataTime;
+            this.setCurrentAnnotation();
+            this.playTrack();
+        })
+        const onLoadedMetaData = (event) => {
+            const duration = this.mainTrack.duration;
+            console.log(duration);
+            this.showHoursMinutesSeconds(this.getParsedDuration(duration));
+        };
+        
+        if (this.mainTrack.duration) {
+            console.log('Track already loaded!');
+            onLoadedMetaData();
+        } else { 
+            this.mainTrack.onloadedmetadata = onLoadedMetaData
+        }
+    },
+    
+    playTrack(){
+        this.mainTrack.play();
+        this.play.style.display = 'none';
+        this.pause.style.display = 'block';
+    },
 
-const annotations = [{
-    time: '09:13',
-    note: 'It speeds up here unexpectedly.'
- },{
-    time: '14:23',
-    note: 'Listen for the change here. The strings fade out.'
- }];
+    pauseTrack(){
+        this.mainTrack.pause();
+        this.play.style.display = 'block';
+        this.pause.style.display = 'none';
+    },
+
+    getParsedDuration(time){
+        const hours = this.getZeroNumber(Math.floor(time / 60 / 60));
+        const minutes = this.getZeroNumber(Math.floor(time / 60) - (hours * 60));
+        const seconds = this.getZeroNumber((time % 60).toFixed());
+        console.log(hours, minutes, seconds);
+        const hoursMinutesSeconds = [
+            hours, 
+            minutes, 
+            seconds
+        ];
+        if (hoursMinutesSeconds[0] === '00') hoursMinutesSeconds.shift();
+        return hoursMinutesSeconds.join(':')
+    },
+
+    getZeroNumber(number){
+        number = (number < 10 ? '0' : '') + number;
+        return number;
+    },
+
+    getSeconds(time) {
+        const minutesSeconds = time.split(':') // ['09', '13']
+        const [minutes, seconds] = minutesSeconds;
+        return Number(minutes) * 60 + Number(seconds);
+    },
+
+    showHoursMinutesSeconds(time) {
+        this.trackDuration.innerHTML = `${time}`;
+    },
+
+    renderAnnotations(){ 
+        this.annotations.forEach(({ time, note, link = '' }) => {
+            const secondsTime = this.getSeconds(time);
+            const li = `
+                <li data-time="${secondsTime}">
+                    ${time} <br> ${note}
+                    ${link && `<a href="${link}">link</a>`}
+                </li>`;
+            // let li = document.createElement('li')
+            this.timeStamps.innerHTML += li;
+        });
+    },
+
+    setCurrentAnnotation(){
+        const annotations = [...this.annotations].reverse()
+        //console.log(annotations);
+        // loop over annotations and find the first where time < current time.
+        const currentTime = this.mainTrack.currentTime;
+        
+        // Remove active class
+        const active = document.querySelector('#time-stamps li.active')
+        console.log(active);
+        if (active) {
+            active.classList.remove('active')
+        } 
+
+        // Add active class
+        for (let { time } of annotations) {
+            const annotationTime = this.getSeconds(time)
+            if (currentTime < annotationTime) continue;
+            const annotation = document.querySelector(`[data-time="${annotationTime}"]`)
+            annotation.classList.add('active')
+            break;
+        }
+    },
+
+    setCurrentAnnotationInterval(){
+        setInterval(() => this.setCurrentAnnotation(), 500);
+    }
+}
 
 /*const tracks = [{
     name: '',
@@ -27,68 +161,6 @@ annotations.forEach((annotation) => {
     const { time, note } = annotation; // ES6
 })*/
 
-function getSeconds(time) {
-    // (min * 60) + seconds
-    // 00:00:00
-    const minutesSeconds = time.split(':') // ['09', '13']
-    const [minutes, seconds] = minutesSeconds;
-    return Number(minutes) * 60 + Number(seconds);
-}
+Program.init();
 
-annotations.forEach(({ time, note }) => {
-    const secondsTime = getSeconds(time);
-    const li = `<li data-time="${secondsTime}">${time} <br> ${note}</li>`;
-    // let li = document.createElement('li')
-    timeStamps.innerHTML += li;
-});
-
-const click = 'click'
-play.addEventListener(click, (event) => {
-    // press play and hide play and show pause
-    mainTrack.play();
-    play.style.display = 'none';
-    pause.style.display = 'block';
-})
-
-pause.addEventListener(click, (event) => {
-    // press play and hide play and show pause
-    mainTrack.pause();
-    play.style.display = 'block';
-    pause.style.display = 'none';
-})
-
-
-timeStamps.addEventListener(click, (event) => {
-    console.log(event, event.target);
-    //event.target.dataset.time;
-    const dataTime = event.target.getAttribute('data-time');
-    mainTrack.currentTime = dataTime;
-    mainTrack.play();
-    play.style.display = 'none';
-    pause.style.display = 'block';
-})
-
-function showHoursMinutesSeconds(time) {
-    const hours = Math.floor(time / 60 / 60);
-    const minutes = Math.floor(time / 60) - (hours * 60);
-    const seconds = (time % 60).toFixed();
-    let formattedTime
-    if (hours === 0) { 
-        formattedTime = minutes + ':' + seconds;
-    } else { 
-        formattedTime = hours + ':' + minutes + ':' + seconds;
-    }
-    trackDuration.innerHTML = `${formattedTime}`;
-}
-
-function getDuration() {
-    // Wait until file metadata is loaded
-    mainTrack.onloadedmetadata = (event) => {
-        const duration = mainTrack.duration;
-        console.log(duration);
-        showHoursMinutesSeconds(duration);
-    };
-}
-
-getDuration();
 
